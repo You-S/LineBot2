ecc8fe5b4bfaf95a686948a48281b8e2840a100f
YouS <0306you10@gmail.com>
2021-03-17 22:17:34 +0900

    10

--- app.py
+++ app.py
@@ -89,5 +89,4 @@ def check(carName):
     return bookList    
     
 if __name__ == "__main__":
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
